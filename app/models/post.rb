2ea2813231c0c0e7a31743ede58bfce700e8035b
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-04-07 18:32:13 +0100

    commit tree ce586ab9db4bca23a8a21aa19c561fb383f2d555

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ['Fiction', 'Non-Fiction']
+   validate :title_is_clickbait
+
+    def title_is_clickbait
+        unless title =~ /Won't Believe|Secret|Top \d|Guess/
+            errors.add(:title, "Title must be clickbait")
+        end 
+    end
+
 end
